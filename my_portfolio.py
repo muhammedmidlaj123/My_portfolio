3b0377d566a98d32d64d1847ba93eb9ef517b965
muhammedmidlaj123 <muhammedmidlaj2021@gmail.com>
2026-01-12 13:32:15 +0400

    Update my_portfolio.py

--- my_portfolio.py
+++ my_portfolio.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# import google.generativeai as genai
+ import google.generativeai as genai
 
 # 1. Main Title
 st.title("Hi, I am Midhilaj EK! 👋")
@@ -72,7 +72,7 @@ api_key = st.text_input("Enter your Google API Key to Chat:", type="password")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash')
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
